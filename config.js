500c051f46197d53a5a67cac06498584a718c6bd
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-10-08 13:18:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-global.you = ['62895604670507'] // Developer Bot
-global.rowner = ['62895604670507'] // Real Owner Bot
-global.owner = ['62895604670507'] // Owner Bot
-global.police = ['62895604670507'] // Police Bot
-global.mods = ['62895604670507']  // Want some help?
-global.prems = ['62895604670507'] // Premium user has unlimited limit
-global.helper = ['62895604670507']
+global.you = ['6282365089294'] // Developer Bot
+global.rowner = ['6282365089294'] // Real Owner Bot
+global.owner = ['6282365089294'] // Owner Bot
+global.police = ['6282365089294'] // Police Bot
+global.mods = ['6282365089294']  // Want some help?
+global.prems = ['6282365089294'] // Premium user has unlimited limit
+global.helper = ['6282365089294']
 global.APIs = { // API Prefix
   // name: 'https://website'
   amel: 'https://melcanz.com',
@@ -59,9 +59,9 @@ global.useMulti = false
 global.media = 'https://telegra.ph/file/da17ba93a26ee0a9b1059.jpg'
 
 // Sticker WM
-global.packname = 'Xiao'
-global.author = 'Zens'
-global.wm = '© Xiao BOT'
+global.packname = 'Created By'
+global.author = 'XYZE BOT'
+global.wm = '© XYZE BOT'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
